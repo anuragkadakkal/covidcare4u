99a4a1d0778b6ff5ee069adc9dd357f6f7061061
Stella M Thomas <anuraga2021@mca.ajce.in>
2021-05-13 23:00:43 +0530

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -24,7 +24,6 @@
   		$response = file_get_contents($url, false, $context);
 
 		$res = json_decode($response, true);
-		echo $res['success'];exit;
 		if($res['success'] == true)
 		{
 			$sql="insert into tb_logginglogin(logtoken,loginusername,curdate) values('".$_POST['token']."','".$usr."','".date("d-m-Y h:i:sa")."')";
@@ -45,53 +44,39 @@
 				if($d==1)
 				{
 					setcookie("lkey",$b);
+					setcookie("logined",1);
 					if ($c==0)
 					{
-						$_SESSION["logined"] = 1;
-						header("location:admin/adminhome.php"); 
-						/*Admin SESSION - Completed - Validation Completed*/
+						header("location:admin/adminhome.php");
 					}
 					else if($c==1)
 					{
-						setcookie("logined",1);
 						header("location:customerhome.php");
-						/*Customer SESSION - Completed - Validation Completed*/
 					}
 					else if($c==2)
 					{
-						$_SESSION["logined"] = 1;
-						header("location:police/policehome.php"); 
-						/*Police Station SESSION - Completed - Validation Completed*/
+
+						header("location:police/policehome.php");
 					}
 					else if($c==3)
 					{
-						$_SESSION["logined"] = 1;
 						header("location:kitchen/kitchenhome.php");
-						/*Community SESSION - Completed - Validation Completed*/
 					}
 					else if($c==4)
 					{
-						$_SESSION["logined"] = 1;
-						header("location:karunyamedicals/medicalhome.php"); 
-						/*Karunya Medicals SESSION - Completed - Validation Completed*/
+						header("location:karunyamedicals/medicalhome.php");
 					}
 					else if($c==5)
 					{
-						$_SESSION["logined"] = 1;
 						header("location:phc/phchome.php");
-						/*PHC SESSION - Completed - Validation Completed*/
 					}
 					else if($c==6)
 					{
-						$_SESSION["logined"] = 1;
-						header("location:doctor/drhome.php"); 
-						/*Doctor SESSION - Completed - Validation Completed*/
+						header("location:doctor/drhome.php");
 					}
 					else if($c==7)
 					{
-						$_SESSION["logined"] = 1;
-						header("location:ambulance/ambulancehome.php"); 
-						/*Ambulance SESSION - Completed - Validation Completed*/
+						header("location:ambulance/ambulancehome.php");
 					}
 					else{}
 				}
